d2a47b57cfc6fa4d5fafaa07e6179691b33211a5
Amon Cardim <amoncardim@gmail.com>
2025-07-02 11:14:23 -0300

    created entitys and add anotation in principal application

---
+++ src/main/java/com/amoncardim/alunos/entity/Aluno.java
@@ -0,0 +1,26 @@
+package com.amoncardim.alunos.entity;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Aluno extends BaseEntity{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private LocalDate dataNascimento;
+
+    private List<Matricula> matriculas = new ArrayList<>();
+}
